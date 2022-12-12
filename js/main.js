eb67a5dda3867ee26c33ea3d42aa9a799769847e
Mamokgolwane Moloi <108424497+mamsieCodess@users.noreply.github.com>
2022-12-12 18:12:52 +0200

    Update

--- js/main.js
+++ js/main.js
@@ -1,20 +1,20 @@
-let index = [1,1];
+let index = [1, 1];
 let slideId = ["slide1", "slide2"]
 
 showSlide(1, 0);
 showSlide(1, 1);
 
 function plusSlide(n, no) {
-  showSlide(index[no] += n, no);
+    showSlide(index[no] += n, no);
 }
 
 function showSlide(n, no) {
-  let i;
-  let x = document.getElementsByClassName(slideId[no]);
-  if (n > x.length) {index[no] = 1}    
-  if (n < 1) {index[no] = x.length}
-  for (i = 0; i < x.length; i++) {
-     x[i].style.display = "none";  
-  }
-  x[index[no]-1].style.display = "block";  
+    let i;
+    let x = document.getElementsByClassName(slideId[no]);
+    if (n > x.length) { index[no] = 1 }
+    if (n < 1) { index[no] = x.length }
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
+    }
+    x[index[no] - 1].style.display = "block";
 }
